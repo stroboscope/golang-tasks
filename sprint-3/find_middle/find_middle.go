65b8864977d156f67ac6959010eb281f74b8c134
stroboscope <stroboscope@akmr.ru>
2025-12-13 02:42:20 +0300

    find_middle

--- sprint-3/find_middle/find_middle.go
+++ sprint-3/find_middle/find_middle.go
@@ -6,6 +6,38 @@ type Node struct {
 }
 
 func FindMiddle(head *Node) *Node {
-	// TODO: implement me
-	return nil
+	var p1 *Node
+	var p2 *Node
+
+	var i1 int
+	var i2 int
+	var s int
+
+	if head == nil {
+		return nil
+	}
+
+	if head.Next != nil {
+		p1 = head
+		p2 = head.Next
+	} else {
+		return head
+	}
+
+	for {
+		s = i2/2 - i1 + 1
+		i1 += s
+
+		for i := 0; i < s; i++ {
+			p1 = p1.Next
+		}
+
+		if p2.Next == nil {
+			return p1
+		}
+
+		p2 = p2.Next
+		i2++
+
+	}
 }
