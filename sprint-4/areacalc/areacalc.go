c4bb47930cfcf3215b287cb3611444d440c93ee2
stroboscope <stroboscope@akmr.ru>
2026-01-03 05:52:03 +0300

    areacalc.go

--- sprint-4/areacalc/areacalc.go
+++ sprint-4/areacalc/areacalc.go
@@ -1,5 +1,9 @@
 package areacalc
 
+import (
+	"strings"
+)
+
 const pi = 3.14159
 
 type Shape interface {
@@ -8,26 +12,45 @@ type Shape interface {
 }
 
 type Rectangle struct {
-	// TODO: implement me
+	name string
+	side []float64
 }
 
-func NewRectangle(float64, float64, string) *Rectangle {
-	// TODO: implement me
-	return &Rectangle{}
+func NewRectangle(a, b float64, s string) *Rectangle {
+	return &Rectangle{name: s, side: []float64{a, b}}
 }
 
-// TODO: implement me
+func (r *Rectangle) Area() float64 {
+	return r.side[0] * r.side[1]
+}
+
+func (r *Rectangle) Type() string {
+	return r.name
+}
 
 type Circle struct {
-	// TODO: implement me
+	name   string
+	radius float64
 }
 
-func NewCircle(float64, string) *Circle {
-	// TODO: implement me
-	return &Circle{}
+func NewCircle(r float64, s string) *Circle {
+	return &Circle{radius: r, name: s}
+}
+
+func (c *Circle) Area() float64 {
+	return pi * c.radius * c.radius
+}
+
+func (c *Circle) Type() string {
+	return c.name
 }
 
 func AreaCalculator(figures []Shape) (string, float64) {
-	// TODO: implement me
-	return "", 0.0
+	var sum float64
+	var desc []string
+	for _, f := range figures {
+		sum += f.Area()
+		desc = append(desc, f.Type())
+	}
+	return strings.Join(desc, "-"), sum
 }
